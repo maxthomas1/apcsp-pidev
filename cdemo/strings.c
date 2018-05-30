ac66e97016e865d77645372164bc9640e76eb821
maxthomas1 <38955641+maxthomas1@users.noreply.github.com>
2018-05-29 20:55:01 -0700

    Update strings.c

--- cdemo/strings.c
+++ cdemo/strings.c
@@ -1,32 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 
+
+
 int main()
 {
-	char str[] = "abcdefghijklmnopqrstuvwxyz";
-	char str2[27];
-	char str3[53];
-	char c ='a';
-	for (int i = 0; i < 26; i++)
-	{
-		str2[i] = str[0] +i;
-	}
-	if (strcmp(str, str2) == 0)
-		printf("The strings are identical\n");
-	else
-		printf("The strings are different\n");
-	printf("%s\n %s\n", str, str2);
 
-	for (int i = 0; i < 26; i++)
-	{
-		str[i] = str[i] - 32;
-	}
-	if (strcmp(str2, str3) == 0)
-		printf("The strings are identical\n");
-	else
-		printf("The strings are different\n");
-	strcpy(str3, str);
-	strcat(str3, str2);
-	printf("%s + %s = %s\n", str, str2, str3);
+  char str1[27];
+  char c = 'a';
 
+  for (int i = 0; i < 26; i++)
+    {
+      str1[i] = c;
+      c++;
+    }
+
+  str1[26] = '\0';
+  
+  char str2[] = "abcdefghijklmnopqrstuvwxyz";
+  char str3[53] = "";
+
+  if (strcmp(str1, str2) == 0)
+    {
+      printf("The strings are identical.\n");
+    }
+  else
+    {
+      printf("The strings are different.\n");
+    }
+  
+  for (int i = 0; i < 26; i++)
+    {
+      str1[i] = str1[i] - 32;
+    }
+
+  if (strcmp(str1, str2) == 0)
+    {
+    printf("The strings are identical.\n");
+    }
+  else
+    {
+    printf("The strings are different.\n");
+    }
+
+  strcpy(str3, str1);
+  strcat(str3, str2);
+
+  printf("%s\n", str1);
+  printf("%s\n", str2);
+  printf("%s\n", str3);
+  
 }
